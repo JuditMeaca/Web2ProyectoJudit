67348de9cca70b5a06045ca7878b79edcfd3dad6
Judit Meaca <juditmeaca@gmail.com>
2020-05-29 23:17:26 -0300

    realizando los modelos

--- models/public.model.php
+++ models/public.model.php
@@ -2,4 +2,23 @@
 
 class PublicModel{
     
+    private function createConetion(){
+        $host='localhost';
+        $username='root';
+        $password='';
+        $database= 'db_limpieza';
+
+        try{
+            $pdo= new PDO("mysql:host=$host;dbname=$database;charset=utf8, $username,$password");
+            $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_WARNING);    
+        }
+        catch (Exception $e){
+            var_dump($e);
+        }
+        return $pdo;
+    }
+
+    public function getDetails(){
+        $db=$this->createConetion();
+    }
 }
